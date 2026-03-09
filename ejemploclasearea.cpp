a91893d9de743944e917b58f8eca454712b8b1b1
sofiaolmospino <a2025112327@estudiantes.upsa.edu.bo>
2026-03-09 13:29:47 -0400

    Implement main function to calculate triangle area

---
+++ ejemploclasearea.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "Triangulo.h"
+using namespace std;
+
+int main()
+{
+    Triangulo triangulo;
+    cout << "El area del triangulo es: "<< triangulo.calculararea(5, 6);
+    return 0;
+}
